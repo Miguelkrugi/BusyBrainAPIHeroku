5d28af2db6bfba034e0b36b437dbb463ad84543f
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-11-05 16:19:26 +0000

    added post method utilizador
    added post method to register a user on the app

--- src/main/java/com/busybrain/api/prototipo/controllers/UtilizadorController.java
+++ src/main/java/com/busybrain/api/prototipo/controllers/UtilizadorController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,13 @@ public class UtilizadorController {
         return utilizadorRepository.findAll();
     }
 
-    
+    @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Utilizador saveUtilizador(@RequestBody Utilizador utilizador){
+
+        Utilizador savedUtilizador = utilizadorRepository.save(utilizador);
+        logger.info("Saving user...");
+        return savedUtilizador;
+
+    }
 
 }
