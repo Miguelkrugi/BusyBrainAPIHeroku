04f04cb74f5e4f3cc29429852c63bad139646054
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-11-05 17:02:34 +0000

    exceptions and searchuserby username

--- src/main/java/com/busybrain/api/prototipo/controllers/UtilizadorController.java
+++ src/main/java/com/busybrain/api/prototipo/controllers/UtilizadorController.java
@@ -1,6 +1,7 @@
 package com.busybrain.api.prototipo.controllers;
 
 import com.busybrain.api.prototipo.models.Utilizador;
+import com.busybrain.api.prototipo.models.exceptions.NotFoundException;
 import com.busybrain.api.prototipo.models.repositories.UtilizadorRepository;
 
 import org.slf4j.Logger;
@@ -26,6 +27,17 @@ public class UtilizadorController {
         logger.info("A Exibir os utilizadores");
         return utilizadorRepository.findAll();
     }
+
+    @GetMapping(path = "{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Utilizador getUtilizador(@PathVariable("username") String username) throws NotFoundException {
+        logger.info("Sending user with username " + username);
+        Utilizador utilizador = (Utilizador) utilizadorRepository.findByUsername(username));
+        if (utilizador != null)
+            return utilizador;
+        else
+            throw new NotFoundException("" + username, "Utilizador", "username");
+    }
+
 
     @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public Utilizador saveUtilizador(@RequestBody Utilizador utilizador){
---
+++ src/main/java/com/busybrain/api/prototipo/models/exceptions/NotFoundException.java
@@ -0,0 +1,17 @@
+package com.busybrain.api.prototipo.models.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException{
+    
+    private static final long serialVersionUID=5405519104069955535L;
+
+    public NotFoundException(String username, String elemType, String idName) {
+        super(elemType+" with "+idName+" "+username+" not found.");
+    }  
+
+    
+
+}
