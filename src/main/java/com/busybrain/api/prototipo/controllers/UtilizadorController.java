6422635b69d7aacf29e938ceed845fe6ee175351
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-11-05 18:36:47 +0000

    t11

--- src/main/java/com/busybrain/api/prototipo/controllers/UtilizadorController.java
+++ src/main/java/com/busybrain/api/prototipo/controllers/UtilizadorController.java
@@ -46,7 +46,7 @@ public class UtilizadorController {
      }
 
     @GetMapping(path = "/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Utilizador getUtilizadorUsername(@PathVariable("username") String username){
+    public Utilizador getUtilizadorUsername(@PathVariable(value = "username") String username){
 
          logger.info("Sending user with username: " + username);
 
