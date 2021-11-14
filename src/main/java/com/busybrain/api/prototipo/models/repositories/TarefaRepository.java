8e980d6408330e25046420395c85ef5b6f02f043
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-11-14 19:02:28 +0000

    c7

--- src/main/java/com/busybrain/api/prototipo/models/repositories/TarefaRepository.java
+++ src/main/java/com/busybrain/api/prototipo/models/repositories/TarefaRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface TarefaRepository extends CrudRepository<Tarefa, Integer>{
     
-    Iterable<Tarefa> findByType(int typeid);
+    Iterable<Tarefa> findByTypeid(int typeid);
 
 
 
