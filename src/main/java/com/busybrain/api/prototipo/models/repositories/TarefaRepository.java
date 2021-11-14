474b67b7a69095bfc89312a5b6c86375e02835e7
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-11-14 18:53:24 +0000

    c5

--- src/main/java/com/busybrain/api/prototipo/models/repositories/TarefaRepository.java
+++ src/main/java/com/busybrain/api/prototipo/models/repositories/TarefaRepository.java
@@ -8,7 +8,7 @@ public interface TarefaRepository extends CrudRepository<Tarefa, Integer>{
     
     Iterable<Tarefa> findByType(int typeid);
 
-    Iterable<Tarefa> findByUserid(int id);
+
 
     Iterable<Tarefa> findByTitleContaining(String title);
 
