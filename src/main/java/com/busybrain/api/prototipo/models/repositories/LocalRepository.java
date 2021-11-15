61a3ea87d16b597071e8d9d612840f7fa97269b2
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-11-15 03:09:32 +0000

    local by category

--- src/main/java/com/busybrain/api/prototipo/models/repositories/LocalRepository.java
+++ src/main/java/com/busybrain/api/prototipo/models/repositories/LocalRepository.java
@@ -2,10 +2,14 @@ package com.busybrain.api.prototipo.models.repositories;
 
 import com.busybrain.api.prototipo.models.Local;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LocalRepository extends CrudRepository<Local, Integer>{
  
     public Iterable<Local> findByNameContaining(String name);
+
+    @Query(value = "SELECT * FROM place WHERE place_categoria = '2'", nativeQuery = true)
+    Iterable<Local> findByCategoria();
 
 }
