cba6a573554a8a1244def5a6f34d73c0198e9983
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-11-29 10:45:09 +0000

    ch82

--- src/main/java/com/busybrain/api/prototipo/models/repositories/MarcacaoFavoritoRepository.java
+++ src/main/java/com/busybrain/api/prototipo/models/repositories/MarcacaoFavoritoRepository.java
@@ -9,11 +9,13 @@ public interface MarcacaoFavoritoRepository extends CrudRepository<MarcacaoFavor
     
     public Iterable<MarcacaoFavorito> findByUserid(int userid);
 
-    @Query(value = "select favorite_id, user_name, place_name from marcacao_favorito inner join utilizador on user_id = utilizador_id inner join place on local_id = place_id where isfavorite = '1'", nativeQuery = true)
+    String queryByFavorite = "select favorite_id, user_name, place_name from marcacao_favorito inner join utilizador on user_id = utilizador_id inner join place on local_id = place_id where isfavorite = '1'";
+
+    @Query(value = queryByFavorite, nativeQuery = true)
     Iterable<MarcacaoFavorito> findByFavoritestatus();
 
 
-   // select favorite_id, user_name, place_name 
+// select favorite_id, user_name, place_name 
 //from marcacao_favorito
 //inner join utilizador on user_id = utilizador_id
 //inner join place on local_id = place_id
