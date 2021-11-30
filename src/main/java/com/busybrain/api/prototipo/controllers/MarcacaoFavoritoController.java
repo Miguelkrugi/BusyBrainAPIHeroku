1b8bcf92288788aab7dcf28ba7d39087b5c0f928
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-11-30 02:52:13 +0000

    ch109

--- src/main/java/com/busybrain/api/prototipo/controllers/MarcacaoFavoritoController.java
+++ src/main/java/com/busybrain/api/prototipo/controllers/MarcacaoFavoritoController.java
@@ -50,7 +50,8 @@ public class MarcacaoFavoritoController {
 
     }
 
-    //MARCAR LOCAIS FAVORITOS
+    //MARCAR LOCAIS FAVORITOS -- TERMINAR AMANHA (UPDATE A VALORES BOOLEANOSch10)
+    
 
     @PutMapping(path = "/updateplace/{favorite_id}")
     public MarcacaoFavorito atualizarFavorito(@PathVariable(value = "favorite_id") int id, @RequestBody MarcacaoFavorito marcacaoFavorito){ //O request body recebe os dados de uma tarefa a atualizar
