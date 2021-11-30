57524711a5435c515b3f72412fe7c7198c04fad1
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-11-30 22:46:10 +0000

    ch116

--- src/main/java/com/busybrain/api/prototipo/models/repositories/ConvivioRepository.java
+++ src/main/java/com/busybrain/api/prototipo/models/repositories/ConvivioRepository.java
@@ -1,10 +1,20 @@
 package com.busybrain.api.prototipo.models.repositories;
 
 import com.busybrain.api.prototipo.models.Convivio;
+import com.busybrain.api.prototipo.views.ConvivioView;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ConvivioRepository extends CrudRepository<Convivio, Integer>{
 
+    String queryByGrupo = "select convivios.convivio_id AS Idconvivio, convivios.data_convivio AS Dataconvivio, groupss.group_name AS Nomegrupo, placess.place_name AS Nome " +
+    "from convivio AS convivios " +
+    "inner join grupo AS groupss on groupss.group_id = convivios.grupo_id " +
+    "inner join place AS placess on placess.place_id = convivios.placee_id ";
+
+    @Query(value = queryByGrupo + "where groupss.group_id=:grupoid", nativeQuery = true)
+    Iterable<ConvivioView> findByGroupId(@Param("grupoid") int grupoid);
 
 }
--- src/main/java/com/busybrain/api/prototipo/views/ConvivioView.java
+++ src/main/java/com/busybrain/api/prototipo/views/ConvivioView.java
@@ -1,5 +1,12 @@
 package com.busybrain.api.prototipo.views;
 
+import java.sql.Date;
+
 public interface ConvivioView {
     
+    int getIdConvivio();
+    Date getDataConvivio();
+    String getNomeGrupo();
+    String getNome();
+
 }
