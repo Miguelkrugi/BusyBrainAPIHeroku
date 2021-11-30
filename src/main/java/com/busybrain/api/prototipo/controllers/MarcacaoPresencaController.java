3e81960266f8dea69023ebfeaca54a54eb89e2a9
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-11-30 10:38:31 +0000

    ch109

--- src/main/java/com/busybrain/api/prototipo/controllers/MarcacaoPresencaController.java
+++ src/main/java/com/busybrain/api/prototipo/controllers/MarcacaoPresencaController.java
@@ -6,10 +6,13 @@ import com.busybrain.api.prototipo.views.MarcacaoPresencaView;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +35,16 @@ public class MarcacaoPresencaController {
         logger.info("Sending all favorite places from user with id: " + userid);
 
         return marcacaoPresencaRepository.findPresencasByUserid(userid);
+
+    }
+
+    @PutMapping(path = "/updateplace/{presenca_id}")
+    public MarcacaoPresenca atualizarPresenca(@PathVariable(value = "presenca_id") int preid, @RequestBody MarcacaoPresenca marcacaoPresenca){ //O request body recebe os dados de uma tarefa a atualizar
+
+        MarcacaoPresenca marcacaopreAtual = marcacaoPresencaRepository.findById(preid).get(); //Aceder o ID da marcacao a atualizar (marcacao ou nao marcacao de "favorito")
+
+        BeanUtils.copyProperties(marcacaoPresenca, marcacaopreAtual, "id"); //Quais sejam os dados atualizados, ele irá guardar esses novos dados (excepto o "ID", pois não pode ser alterado)
+        return marcacaoPresencaRepository.save(marcacaopreAtual); //Guardar uma nova tarefa com os novos dados requisitados pelo utilizador através do "input" tarefa (no @RequestBody)
 
     }
     
