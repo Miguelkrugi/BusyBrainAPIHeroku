149e3d3b9138a5c8973f69217d2a105d3f2d48af
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-12-01 01:59:59 +0000

    ch121

--- src/main/java/com/busybrain/api/prototipo/controllers/UtilizadorTarefaController.java
+++ src/main/java/com/busybrain/api/prototipo/controllers/UtilizadorTarefaController.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,25 @@ public class UtilizadorTarefaController {
         return utilizadorTarefaRepository.findAllUtilizadorTarefa();
     }
 
+    //ADICIONAR UM UTILIZADOR A UM GRUPO (ADICIONAR A UMA TAREFA)
+
+    @PostMapping(path = "/addparticipant", produces = MediaType.APPLICATION_JSON_VALUE)
+    public UtilizadorTarefa saveParticipant(@RequestBody UtilizadorTarefa utilizadorTarefa){
+
+      UtilizadorTarefa savedUtilizadorTarefa = utilizadorTarefaRepository.save(utilizadorTarefa);
+
+      logger.info("Saving participant...");
+      return savedUtilizadorTarefa;
+
+    }
+
+    /*@PostMapping(path = "/createtask", produces = MediaType.APPLICATION_JSON_VALUE)  //COMPLETO
+    public Tarefa saveTarefa(@RequestBody Tarefa tarefa){
+
+       Tarefa savedTarefa = tarefaRepository.save(tarefa);
+       logger.info("Saving tarefa...");
+       return savedTarefa;
+
+    }*/
 
 }
