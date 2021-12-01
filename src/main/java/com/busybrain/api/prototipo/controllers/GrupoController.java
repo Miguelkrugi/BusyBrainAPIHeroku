534e0407542af4c33416f80d1254a41bfc5187e2
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-12-01 16:02:02 +0000

    ch125

--- src/main/java/com/busybrain/api/prototipo/controllers/GrupoController.java
+++ src/main/java/com/busybrain/api/prototipo/controllers/GrupoController.java
@@ -3,11 +3,14 @@ package com.busybrain.api.prototipo.controllers;
 import com.busybrain.api.prototipo.models.Grupo;
 import com.busybrain.api.prototipo.models.repositories.GrupoRepository;
 
+import org.apache.catalina.connector.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +42,24 @@ public class GrupoController {
 
     }
 
+    //Apagar um grupo
     
+    @DeleteMapping(path = "/deletegroup/{group_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void deleteGroup(@PathVariable("group_id") int id){
+
+        logger.info("Deleting group with id: " + id);
+
+        grupoRepository.deleteById(id);
+
+    }
+
+   /* @DeleteMapping(path = "/deletetask/{task_id}", produces = MediaType.APPLICATION_JSON_VALUE) //COMPLETO
+    public void deleteTarefa(@PathVariable("task_id") int id){
+
+         logger.info("Deleting task with id: " + id);
+
+         tarefaRepository.deleteById(id);
+
+    }*/
 
 }
