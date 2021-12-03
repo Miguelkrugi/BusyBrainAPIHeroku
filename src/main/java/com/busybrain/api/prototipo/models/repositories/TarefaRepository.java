5a2dbcaed9bd88e8c00cb57b20229a6ee1a46e09
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-12-03 00:11:11 +0000

    ch128

--- src/main/java/com/busybrain/api/prototipo/models/repositories/TarefaRepository.java
+++ src/main/java/com/busybrain/api/prototipo/models/repositories/TarefaRepository.java
@@ -13,10 +13,11 @@ public interface TarefaRepository extends CrudRepository<Tarefa, Integer>{
     Iterable<Tarefa> findTaskByUsertaskid(int usertaskid);
     Iterable<Tarefa> findByPriority(int priority);
 
-    String queryByTaskCardview = "select tarefas.task_title AS taskTitle, tarefas.task_desc AS taskDesc, priority.taskpriority_type AS prioridade " +
+    String queryByTaskCardview = "select tarefas.task_id AS taskId, tarefas.task_title AS taskTitle, tarefas.task_desc AS taskDesc, priority.taskpriority_type AS prioridade " +
     "from tarefa AS tarefas " + 
     "inner join prioridadetarefa priority on priority.taskpriority_id = tarefas.task_priority_id " + 
-    "inner join tipotarefa tipo on tarefas.task_type_id = tipo.tasktype_id";
+    "inner join tipotarefa tipo on tarefas.task_type_id = tipo.tasktype_id " + 
+    "order by taskId";
 
     @Query(value = "SELECT * FROM tarefa WHERE task_priority = '3'", nativeQuery = true)
     Iterable<Tarefa> findTaskByPriority();
