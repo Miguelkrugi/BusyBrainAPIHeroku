fc90e20adac5dda354ed95a02021da53d653ebd7
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-12-03 01:26:23 +0000

    ch130

--- src/main/java/com/busybrain/api/prototipo/views/TarefaView.java
+++ src/main/java/com/busybrain/api/prototipo/views/TarefaView.java
@@ -2,7 +2,6 @@ package com.busybrain.api.prototipo.views;
 
 public interface TarefaView {
     
-    int getTaskId();
     String getTaskTitle();
     String getTaskDesc();
     String getPrioridade();
