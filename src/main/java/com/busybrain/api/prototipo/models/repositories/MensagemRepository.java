a544b59f99059753a813a74354389d6b86c82358
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-12-09 00:13:15 +0000

    168

--- src/main/java/com/busybrain/api/prototipo/models/repositories/MensagemRepository.java
+++ src/main/java/com/busybrain/api/prototipo/models/repositories/MensagemRepository.java
@@ -10,13 +10,12 @@ import org.springframework.data.repository.query.Param;
 public interface MensagemRepository extends CrudRepository<Mensagem, Integer>{
     
     String queryToMessage = "select mensagens.message_content AS messageContent, users.user_name AS Username, grupos.group_name AS groupName " + 
-    "from mensagem AS mensagens " +  
-    "inner join utilizador users on users.user_id = mensagens.message_user_id " + 
+    "from mensagem AS mensagens " + 
+    "inner join utilizador_tarefa userstasks on userstasks.user_identifier = mensagens.message_user_id " + 
+    "inner join utilizador users on users.user_id = userstasks.user_identifier " + 
     "inner join chat chats on chats.chat_id = mensagens.message_chat_id " + 
     "inner join grupo grupos on grupos.group_id = chats.chat_id ";
             
-
-
     @Query(value = queryToMessage + "where chats.chat_id=:chatid", nativeQuery = true)
     Iterable<MensagemView> findByChatid(@Param("chatid") int chatid); 
 
