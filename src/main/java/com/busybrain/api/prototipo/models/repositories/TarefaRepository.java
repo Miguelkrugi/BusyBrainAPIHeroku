5dc427e6fb58acffc8685270ffedf0c77a3bed59
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-12-14 20:19:28 +0000

    ch187

--- src/main/java/com/busybrain/api/prototipo/models/repositories/TarefaRepository.java
+++ src/main/java/com/busybrain/api/prototipo/models/repositories/TarefaRepository.java
@@ -24,7 +24,7 @@ public interface TarefaRepository extends CrudRepository<Tarefa, Integer>{
     "inner join prioridadetarefa priority on priority.taskpriority_id = tarefas.task_priority_id " + 
     "inner join tipotarefa tipo on tarefas.task_type_id = tipo.tasktype_id";*/
 
-    String queryByTaskCardview = "select tarefas.task_title AS taskTitle, tarefas.task_desc AS taskDesc, priority.taskpriority_type AS prioridade, tarefas.task_id AS taskId, tarefas.user_task_id AS creatorId " +
+    String queryByTaskCardview = "select tarefas.task_title AS taskTitle, tarefas.task_desc AS taskDesc, priority.taskpriority_type AS prioridade, tarefas.task_id AS taskId, tarefas.user_task_id AS usertaskId " +
     "from tarefa AS tarefas " + 
     "inner join prioridadetarefa priority on priority.taskpriority_id = tarefas.task_priority_id " + 
     "inner join tipotarefa tipo on tarefas.task_type_id = tipo.tasktype_id";
