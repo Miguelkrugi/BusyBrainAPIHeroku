53663dd3eddf86361dda88e18bd6e3dc92ffefe7
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-12-22 14:58:04 +0000

    CHCH

--- src/main/java/com/busybrain/api/prototipo/views/WebsiteeView.java
+++ src/main/java/com/busybrain/api/prototipo/views/WebsiteeView.java
@@ -1,10 +1,12 @@
 package com.busybrain.api.prototipo.views;
 
-public interface WebsiteView {
-    
+//INTERFACE A UTILIZAR PARA OBTER SITES BLOQUEADOS E DESBLOQUEADOS
+
+public interface WebsiteeView {
     
     boolean getBlockedStatus();
     String getNomeWebsite();
     String getDominioWebsite();
+
 
 }
