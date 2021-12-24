0563e7787e621278cfb1d5b97ffde5e6b76b6028
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-12-24 20:37:53 +0000

    c

--- src/main/java/com/busybrain/api/prototipo/models/repositories/MarcacaoFavoritoRepository.java
+++ src/main/java/com/busybrain/api/prototipo/models/repositories/MarcacaoFavoritoRepository.java
@@ -28,14 +28,14 @@ public interface MarcacaoFavoritoRepository extends CrudRepository<MarcacaoFavor
 
 
 
-    
+
     String queryByFavoritePlaces = "select favoritos.favorite_id AS favoriteId, userss.user_id AS userId, locais.place_name AS placeName, locais.place_endereco AS placeEndereco, locais.place_id AS placeId " + 
     "from marcacao_favorito AS favoritos " + 
     "inner join utilizador userss on favoritos.utilizador_id = utilizador.user_id " + 
     "inner join place locais on locais.place_id = favoritos.local_id " + 
     "where favoritos.isfavorite = '1' and ";
 
-    @Query(value = queryByFavoritePlaces + "userss.user_id=:utilizadorid", nativeQuery = true)
+    @Query(value = queryByFavoritePlaces + "userss.user_id=:userid", nativeQuery = true)
     Iterable<MarcacaoFavoritoView> findMarcacaoFavoritoByUserId(@Param("utilizador_id") int userid); 
 
 
